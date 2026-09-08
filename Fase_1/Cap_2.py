9a0930fbc7c1b0b3a4bb4302b28f89fc5813c0cc
Thiago <114115392+thiagobernardosantos@users.noreply.github.com>
2026-09-07 21:36:50 -0300

    Create Cap_2.py

---
+++ Fase_1/Cap_2.py
@@ -0,0 +1,8 @@
+# Lista de números fornecida
+numeros = [10, -7, 105, 44, -11, 82]
+
+# Encontrando o maior número na lista
+maior_numero = max(numeros)
+
+# Exibindo o maior número encontrado
+print("O maior número na lista é:", maior_numero)
