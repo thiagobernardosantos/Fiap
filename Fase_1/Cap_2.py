f097d2b09f104b7ee9bda1fcdba57e21e13d1e0f
Thiago <114115392+thiagobernardosantos@users.noreply.github.com>
2026-09-08 20:19:00 -0300

    Delete Fase_1/Cap_2.py

--- Fase_1/Cap_2.py
+++
@@ -1,8 +0,0 @@
-# Lista de números fornecida
-numeros = [10, -7, 105, 44, -11, 82]
-
-# Encontrando o maior número na lista
-maior_numero = max(numeros)
-
-# Exibindo o maior número encontrado
-print("O maior número na lista é:", maior_numero)
