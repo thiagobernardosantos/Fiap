29e25df46ef708263b7f2f3b32d2665354dc2f3e
Thiago <114115392+thiagobernardosantos@users.noreply.github.com>
2026-09-12 14:32:29 -0300

    Create Operadores_de_atribuicao.py

---
+++ Fase_1/Capitulo_3/Operadores_de_atribuicao.py
@@ -0,0 +1,12 @@
+#atribuição da soma de dois números inteiros
+resultado = 2 + 3
+print("Resultado:", resultado)
+#atribuição de um texto (string)
+nome = "Pedro"
+print("Nome: ", nome)
+nome2 = "Maria"
+#saída f"string" com a variável entre as chaves
+print(f"Olá, {nome2}. Tudo bem?")
+#somando dois ao valor anterior da variável
+resultado += 2
+print("Resultado:", resultado)
