79eeeead9fb29a70758325b88818c47e3b0e5e4b
Thiago <114115392+thiagobernardosantos@users.noreply.github.com>
2026-09-12 20:55:13 -0300

    Create delta3.py

---
+++ Fase_1/Capitulo 3/delta3.py
@@ -0,0 +1,13 @@
+#solicitando os valores de A, B e C
+a = float(input("Informe o valor de A"))
+b = float(input("Informe o valor de B"))
+c = float(input("Informe o valor de C"))
+#cálculo do delta
+delta = b * b - 4 * a *c
+#verificação das condições com elif
+if delta>0.0:
+    #cálculo de 2 valores para x
+elif delta == 0.0:
+    #cálculo de 1 valor para x
+else:
+    #exibição da mensagem
