3f023e2f975564e52a881eb7cf52c0f0e9db3cdb
Thiago <114115392+thiagobernardosantos@users.noreply.github.com>
2026-09-12 14:48:53 -0300

    Create calculadora_type.py

---
+++ Fase_1/Capitulo_3/calculadora_type.py
@@ -0,0 +1,6 @@
+valor1 = input("Por favor, digite o primeiro valor: ")
+valor2 = input("Por favor, digite o segundo valor: ")
+soma = valor1 + valor2
+print("A soma entre os valores é " + soma)
+print(type(valor1))
+print(type(valor2))
