586ef34ab383ab176cf631d290fe0301cb33bc82
Thiago <114115392+thiagobernardosantos@users.noreply.github.com>
2026-09-12 20:55:01 -0300

    Create delta2.py

---
+++ Fase_1/Capitulo 3/delta2.py
@@ -0,0 +1,14 @@
+#solicitando os valores de A, B e C
+a = float(input("Informe o valor de A"))
+b = float(input("Informe o valor de B"))
+c = float(input("Informe o valor de C"))
+#cálculo do delta
+delta = b * b - 4 * a *c
+# verificação das condições com if encadeado
+if delta > 0.0:
+    # cálculo de 2 valores para x
+else:
+    if delta == 0.0:
+        # cálculo de 1 valor para x
+    else:
+        # exibição da mensagem
