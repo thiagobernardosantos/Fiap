8773a0d83478d1888514604cd0641782b9293a97
Thiago <114115392+thiagobernardosantos@users.noreply.github.com>
2026-09-12 20:54:20 -0300

    Create delta.py

---
+++ Fase_1/Capitulo 3/delta.py
@@ -0,0 +1,6 @@
+#solicitando os valores de A, B e C
+a = float(input("Informe o valor de A"))
+b = float(input("Informe o valor de B"))
+c = float(input("Informe o valor de C"))
+#cálculo do delta
+delta = b * b - 4 * a *c
