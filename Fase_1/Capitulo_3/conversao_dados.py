e569b7637e68ff73a425ef4762e78998b4fde004
Thiago <114115392+thiagobernardosantos@users.noreply.github.com>
2026-09-12 14:50:17 -0300

    Create conversao_dados.py

---
+++ Fase_1/Capitulo_3/conversao_dados.py
@@ -0,0 +1,4 @@
+valor1 = input("Por favor, digite o primeiro valor: ")
+valor2 = input("Por favor, digite o segundo valor: ")
+soma = float(valor1) + float(valor2)
+print("A soma entre os valores é {}".format(soma))
