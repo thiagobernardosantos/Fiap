1cc4afd39e1fb69b40c24a2ce11d4290a097439f
Thiago <114115392+thiagobernardosantos@users.noreply.github.com>
2026-09-12 20:49:43 -0300

    Create professor.py

---
+++ Fase_1/Capitulo 3/professor.py
@@ -0,0 +1,8 @@
+#solicitando os dados do aluno
+email_aluno = input("Informe o e-mail do aluno")
+nota_semestral = input("Informe a nota semestral do aluno: ")
+#convertendo a nota para o formato float
+nota_semestral = float(nota_semestral)
+#realizando o teste lógico
+if nota_semestral > 8.5:
+    print("ENVIANDO E-MAIL PARA {}".format(email_aluno))
