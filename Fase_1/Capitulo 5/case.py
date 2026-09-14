4cf159db581a4a5a23ba99bc6f28b7c94d17d065
Thiago <114115392+thiagobernardosantos@users.noreply.github.com>
2026-09-13 22:05:00 -0300

    Create case.py

---
+++ Fase_1/Capitulo 5/case.py
@@ -0,0 +1,12 @@
+match <comparador>:
+    case <valor 1>:
+        <bloco 1>
+    case <valor 2>:
+        <bloco 2>
+    case <valor 3>:
+        <bloco 3>
+    case <valor 4>:
+        <bloco 4>
+    ...
+    case _:
+        <excessão>
